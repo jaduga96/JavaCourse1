fd932b5bf28e0e68a04b7c9fefd198b23da21bba
Ilnar Mukhanov <jaduga96@gmail.com>
2024-02-13 18:30:02 +0300

    test

--- src/PracticalExc/EX13/main.java
+++ src/PracticalExc/EX13/main.java
@@ -4,5 +4,6 @@ public class main {
     public static void main(String[] args) {
         Squirrel s = new Squirrel(new int[] {-2, 2, 4, -6, -2, -1, -2, 1});
         System.out.println(s.maxNuts());
+        System.out.println(s.maxNuts());
     }
 }
